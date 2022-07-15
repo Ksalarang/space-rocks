185aef4d68230d8c62a974c4cbefb22b7fc7106a
ksalarang <anbleidd@gmail.com>
2022-07-15 23:01:17 +0600

    initial setup

--- core/src/main/java/com/diyartaikenov/game/SpaceLevel.java
+++ core/src/main/java/com/diyartaikenov/game/SpaceLevel.java
@@ -1,10 +1,19 @@
 package com.diyartaikenov.game;
 
+import static com.diyartaikenov.game.SpaceRocksGame.HEIGHT;
+import static com.diyartaikenov.game.SpaceRocksGame.WIDTH;
+
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.diyartaikenov.game.base.actors.BaseActor;
 import com.diyartaikenov.game.base.screens.BaseScreen;
 
 public class SpaceLevel extends BaseScreen {
     private SpaceRocksGame spaceGame;
+
+    private BaseActor background;
 
     public SpaceLevel(Game game) {
         super(game);
@@ -13,10 +22,21 @@ public class SpaceLevel extends BaseScreen {
     @Override
     protected void create(Game game) {
         spaceGame = (SpaceRocksGame) game;
+        mainStage.setViewport(new ExtendViewport(WIDTH, HEIGHT));
+        uiStage.setViewport(new ExtendViewport(WIDTH, HEIGHT));
+
+        Texture starfield = new Texture(Gdx.files.internal("starfield.png"));
+        background = new BaseActor(starfield);
+
+        mainStage.addActor(background);
     }
 
     @Override
-    protected void update(float delta) {
+    protected void update(float delta) {}
 
+    @Override
+    public void dispose() {
+        super.dispose();
+        background.dispose();
     }
 }
