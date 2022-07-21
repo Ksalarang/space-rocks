493fea431f8a9c2c7280a275ed5a4ec5d29ef99f
ksalarang <anbleidd@gmail.com>
2022-07-21 06:02:31 +0600

    refactor SpaceRocksGame

--- core/src/main/java/com/diyartaikenov/game/SpaceRocksGame.java
+++ core/src/main/java/com/diyartaikenov/game/SpaceRocksGame.java
@@ -6,17 +6,17 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.diyartaikenov.game.base.BaseGame;
 
 public class SpaceRocksGame extends BaseGame {
-	public static final int WIDTH = 1920;
-	public static final int HEIGHT = 1080;
+	public static int WIDTH = 1920;
+	public static int HEIGHT = 1080;
 
 	@Override
 	public void create() {
 		super.create();
 		BitmapFont font64 = new BitmapFont(Gdx.files.internal("fonts/Arial-64.fnt"));
 		setDefaultFont(font64);
-		Color overColor = new Color(0x99CCFFFF);
-		textButtonStyle.overFontColor = overColor;
-		textButtonStyle.focusedFontColor = overColor;
+		Color overFontColor = new Color(0x99CCFFFF);
+		textButtonStyle.overFontColor = overFontColor;
+		textButtonStyle.focusedFontColor = overFontColor;
 		textButtonStyle.downFontColor = new Color(0x66B2FFFF);
 		setScreen(new MainMenu(this));
 	}
